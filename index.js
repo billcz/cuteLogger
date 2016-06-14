1527432e61f19f19fc25f35574ffaf87360c11ab
billcz <chenz@dtdream.com>
2016-06-14 16:51:15 +0800

    fix: fix path

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
 * @Date:   2016-06-14T16:33:22+08:00
 * @Email:  chenz@dtdream.com
 * @Last modified by:   billcz
-* @Last modified time: 2016-06-14T16:34:13+08:00
+* @Last modified time: 2016-06-14T16:49:49+08:00
 */
 
 
 
-var logger = require('dist/logger.js');
+var logger = require('./dist/logger.js');
 module.exports = logger;
